521a6fa7d7a97a4c9964df006afa582365ba8515
ShoreiTakada <160503699+gao0325ts@users.noreply.github.com>
2024-10-01 10:14:55 +0000

    [Finish] 3-5

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get '/top' => 'homes#top'
   get 'lists/new'
-  get 'lists/edit'
   get 'lists' => 'lists#index'
   post 'lists' => 'lists#create'
   get 'lists/:id' => 'lists#show', as: 'list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
