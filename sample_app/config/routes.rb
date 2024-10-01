63a35456affd64143812ee28a55410c566605e8e
ShoreiTakada <160503699+gao0325ts@users.noreply.github.com>
2024-10-01 11:18:00 +0000

    [Start] 3-7

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post 'lists' => 'lists#create'
   get 'lists/:id' => 'lists#show', as: 'list'
   patch 'lists/:id' => 'lists#update', as: 'update_list'
+  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
